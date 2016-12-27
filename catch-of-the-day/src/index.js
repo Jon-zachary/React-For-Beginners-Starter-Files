75a833b136dabe15469629e3adfd572658b515c9
Jon Zachary <jontzachary@gmail.com>
2016-12-27 00:57:18 -0500

    add solutions through video 7/30

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,9 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App.js';
+
+import './css/style.css';
+import StorePicker from './components/StorePicker.js'
+
+render(<App />,document.querySelector('#main'))
